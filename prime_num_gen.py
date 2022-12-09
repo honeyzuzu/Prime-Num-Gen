fb8e8c19f29154fcf83f030d6298c7527994ad7c
Queenie Sun <91141179+honeyzuzu@users.noreply.github.com>
2022-12-08 21:51:42 -0500

    working code

--- prime_num_gen.py
+++ prime_num_gen.py
@@ -1,53 +1,30 @@
 import math
 
-def prime_over_40(num):
-    prime = num^2 + num + 41
-    return prime
-
-def prime_plus_one(num):
-    prime = 6 * num + 1
-    return prime
-
-def prime_minus_one(num):
-    prime = 6 * num -1
-    return prime
-
-def generation_loop(amt_to_gen):
+def generate_primes(amt_of_numbers):
     prime_nums_list = []
-    for i in range(amt_to_gen):
-        if(i <= 40 ):
-            num1 = (prime_minus_one(i))
-            num2 = (prime_plus_one(i))
-            if num1 not in prime_nums_list:
-                prime_nums_list.append(num1)
-            
-            if num2 not in prime_nums_list:
-                prime_nums_list.append(num2)
+    for i in range(2, amt_of_numbers):
+        for j in range (2, i):
+            if (i % j) ==0:
+                break
         else:
-            num1 = (prime_over_40(i))
-            if num1 not in prime_nums_list:
-                prime_nums_list.append(num1)
-
-        
-    return prime_nums_list   
+            prime_nums_list.append(i)
+    return prime_nums_list
 
 
 if __name__ == '__main__':
-    amt_of_numbers = input("Enter the amount of prime numbers you wish to generate: ")
+    amt_of_numbers = input("Enter the range of prime numbers you wish to generate: ")
     print('\n')
-    print("Okay, printing " + amt_of_numbers + " numbers...")
-    prime_nums_list = generation_loop(int(amt_of_numbers))
-    prime_nums_list.append(2)
-    prime_nums_list.append(3)
+    print("Okay, printing prime numbers between 0 and " + amt_of_numbers + "...")
+    prime_nums_list = generate_primes(int(amt_of_numbers))
+    #prime_nums_list.append(2)
     prime_nums_list.sort()
 
     count = 1
-    for i in range(2, int(amt_of_numbers)+1):
-        print(prime_nums_list[i])
-        count +=1
+    
+    print(prime_nums_list)
         #print('\n')
 
-    print (count)
+    #print (count)
 
 
 
