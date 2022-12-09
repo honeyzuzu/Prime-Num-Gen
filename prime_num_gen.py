454f8f2cf707e8556cf1044a24e16f0d0fef80b4
Queenie Sun <91141179+honeyzuzu@users.noreply.github.com>
2022-12-08 21:40:46 -0500

    trying new prime generator code

--- prime_num_gen.py
+++ prime_num_gen.py
@@ -15,11 +15,19 @@ def prime_minus_one(num):
 def generation_loop(amt_to_gen):
     prime_nums_list = []
     for i in range(amt_to_gen):
-        if(i <= 40):
-            prime_nums_list.append(prime_minus_one(i))
-            prime_nums_list.append(prime_plus_one(i))
+        if(i <= 40 ):
+            num1 = (prime_minus_one(i))
+            num2 = (prime_plus_one(i))
+            if num1 not in prime_nums_list:
+                prime_nums_list.append(num1)
+            
+            if num2 not in prime_nums_list:
+                prime_nums_list.append(num2)
         else:
-            prime_nums_list.append(prime_over_40(i))
+            num1 = (prime_over_40(i))
+            if num1 not in prime_nums_list:
+                prime_nums_list.append(num1)
+
         
     return prime_nums_list   
 
@@ -29,9 +37,12 @@ if __name__ == '__main__':
     print('\n')
     print("Okay, printing " + amt_of_numbers + " numbers...")
     prime_nums_list = generation_loop(int(amt_of_numbers))
+    prime_nums_list.append(2)
+    prime_nums_list.append(3)
+    prime_nums_list.sort()
 
     count = 1
-    for i in range(1, int(amt_of_numbers)+1):
+    for i in range(2, int(amt_of_numbers)+1):
         print(prime_nums_list[i])
         count +=1
         #print('\n')
